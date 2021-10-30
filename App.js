e6265f09d6f14d0284dd99fcc25f206126a682d3
yudai1102jp <yudaiikenoya@gmail.com>
2021-10-30 12:54:46 +0900

    画面遷移

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Tab, Button, Text, View } from 'react-native';
+import { StyleSheet, } from 'react-native';
 import 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import InitiateScreen from './screens/InitiateScreen';
 
@@ -13,7 +14,7 @@ import ListPageScreen from './screens/ListPageScreen';
 import Info_page from './screens/date_page';
 
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 
 
@@ -23,12 +24,18 @@ export default function App() {
 
       <NavigationContainer>
 
-        <Stack.Navigator initialRouteName="main">
+        {/* <Stack.Navigator initialRouteName="main">
           <Stack.Screen name='all' component={ListPageScreen} />
           <Stack.Screen name='Initiate' component={InitiateScreen} />
           <Stack.Screen name='main' component={Info_page} />
 
-        </Stack.Navigator>
+        </Stack.Navigator> */}
+        <Tab.Navigator initialRouteName="main">
+          <Tab.Screen name='all' component={ListPageScreen} />
+          <Tab.Screen name='Initiate' component={InitiateScreen} />
+          <Tab.Screen name='main' component={Info_page} />
+
+        </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
